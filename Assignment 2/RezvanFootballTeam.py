c0f2223f80ae4d97bd65a55cf443143c0f16c75c
amirrashedi0944 <110764458+amirrashedi0944@users.noreply.github.com>
2022-08-13 18:51:34 +0430

    Add files via upload

--- Assignment 2/RezvanFootballTeam.py
+++ Assignment 2/RezvanFootballTeam.py
@@ -3,25 +3,31 @@ rezvanTied = 0
 rezvanLosses = 0
 rezvanPoints = 0
 numberOfGames = 1
-while numberOfGames != 5:
-    print(f'Game {numberOfGames}')
+while numberOfGames != 9:
+    if numberOfGames % 2 == 0:
+        homeAway = "Away game"
+    else:
+        homeAway = "Home game"
+    print(f'Game {numberOfGames} \t{homeAway}')
     game = input("""*****************
     Win     ---> +3 points
     Tied    ---> +1 points
     Lose    ---> +0 points
     Enter the team point in this match: """)
-    if game == "3":
+    if game == "3" or game == "Win" or game == "win":
         rezvanWins += 1
         rezvanPoints += 3
-    elif game == "1":
+    elif game == "1" or game == "Tied" or game == "tied":
         rezvanTied += 1
         rezvanPoints += 1
-    elif game == "0":
+    elif game == "0" or game == "Lose" or game == "lose" :
         rezvanLosses += 1
     else:
         print("Invalid input.")
         numberOfGames -= 1
     numberOfGames += 1
+
+
 print(f'Number of rezvan team wins = {rezvanWins}')
 print(f'Number of rezvan team losses = {rezvanLosses}')
 print(f'Number of rezvan team tied games = {rezvanTied}')
